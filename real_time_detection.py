b3aa5c1fb88df55ba898f9586adda737c300ea11
Ayazmohammad12 <ayazmohammad8270@gmail.com>
2026-02-19 00:03:01 +0530

    Update real_time_detection.py

--- real_time_detection.py
+++ real_time_detection.py
@@ -212,6 +212,7 @@ def about():
     return render_template("about.html")
 
 
-if __name__=="__main__":
-    app.run(host='0.0.0.0',port=5000,threaded=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, threaded=True)
 
