950d8f1d2747294496fb0245e95e028975623e65
Ayazmohammad12 <ayazmohammad8270@gmail.com>
2026-02-18 23:59:19 +0530

    Update real_time_detection.py

--- real_time_detection.py
+++ real_time_detection.py
@@ -213,5 +213,5 @@ def about():
 
 
 if __name__=="__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port, threaded=True)
+    app.run(host='0.0.0.0',port=5000,threaded=True)
+
